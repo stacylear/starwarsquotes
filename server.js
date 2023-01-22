9251de885cf60bd25d2a47288965c435d4816303
Stacy Lear <stacy.lear@gmail.com>
2023-01-21 18:24:06 -0700

    Initial commit

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const MongoClient = require('mongodb').MongoClient
 const app = express();
 
 //LINK TO DATABASE
-MongoClient.connect('mongodb+srv://<username>:<password>@cluster0.muk2ci3.mongodb.net/?retryWrites=true&w=majority', {
+MongoClient.connect('[MongoDB Credentials]', {
     useUnifiedTopology: true })
     .then(client => {
         console.log('Connected to database.')
